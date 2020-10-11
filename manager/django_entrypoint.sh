668d8bc76184a5e39d595ef3379de5d38f182a41
Guy Cole <guycole@gmail.com>
2020-10-10 23:27:30 -0700

    monitoring

--- manager/django_entrypoint.sh
+++ manager/django_entrypoint.sh
@@ -17,7 +17,8 @@ exec gunicorn daring.wsgi:application \
     --name daring \
     --bind unix:gunicorn.sock \
     --workers 3 \
-    --statsd-host=daring_cyclops_graphite_1:8125 \
+    --statsd-host=localhost:9125 \
+    --statsd-prefix=daring \
     --log-level=info \
     --log-file=/var/log/gunicorn/gunicorn.log \
     --access-logfile=/var/log/gunicorn/access.log &
